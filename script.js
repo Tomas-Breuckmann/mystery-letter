c23d9cadc8b4392faab3f47b978313335fc3246e
Tomas Breuckmann <tomas.yoga@yahoo.com>
2021-12-23 11:10:16 -0300

    projeto mistery letter - criterios 5 e 6 - npm test ok

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function criaCarta(){
     let cartaTexto=document.getElementById('carta-texto')
     let conteudo=cartaTexto.value
-    let palavras=conteudo.split(' ')
+    if (conteudo==false){
+        document.getElementById('carta-gerada').innerText='Por favor, digite o conteúdo da carta.'
+    } else {
     // console.log(conteudo)
+    let palavras=conteudo.split(' ')
+    
     // console.log(palavras)
     let cartaGerada= document.getElementById('carta-gerada')
     for (let i=0 ;i<palavras.length;i+=1){
         let item=document.createElement('span')
         item.innerText=palavras[i];
         cartaGerada.appendChild(item)
-        console.log(palavras[i])
+        // console.log(palavras[i])
         
     }
+}
 }
