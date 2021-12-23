0cbe111c9366842e0f1502aad7a8be32aefeea4e
Tomas Breuckmann <tomas.yoga@yahoo.com>
2021-12-23 13:43:12 -0300

    project mistery letter - fim do dia

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-let grupoEstilo = ['newspaper','magazine1','magazine2']
-let grupoTamanho = ['medium','big','reallybig']
-let grupoRotacao = ['rotateleft','rotateright']
-let grupoInclinacao =['skewleft','skewright']
+const grupoEstilo = ['newspaper','magazine1','magazine2']
+const grupoTamanho = ['medium','big','reallybig']
+const grupoRotacao = ['rotateleft','rotateright']
+const grupoInclinacao =['skewleft','skewright']
 
 function selecionaEstilo(){
-    let est = Math.floor(Math.random()*3)
-    let tam = Math.floor(Math.random()*3)
-    let rot = Math.floor(Math.random()*2)
-    let inc = Math.floor(Math.random()*2)
+    // let est = Math.floor(Math.random()*3)
+    // let tam = Math.floor(Math.random()*3)
+    // let rot = Math.floor(Math.random()*2)
+    // let inc = Math.floor(Math.random()*2)
+    let est = parseInt(Math.random()*3)
+    let tam = parseInt(Math.random()*3)
+    let rot = parseInt(Math.random()*2)
+    let inc = parseInt(Math.random()*2)
+
     return [est,tam,rot,inc]
 }
 
-function criaCarta(){
+function apagaCarta(){
     let itens=document.querySelectorAll('.palavra')
     if (itens.length>0){
         for(let i=itens.length-1;i>=0;i-=1){
             itens[i].parentNode.removeChild(itens[i])
         }
     }
+}
+
+function criaCarta(){
+    apagaCarta()
     let cartaTexto=document.getElementById('carta-texto')
     let conteudo=cartaTexto.value
     if (conteudo==false){
@@ -28,8 +37,8 @@ function criaCarta(){
         let cartaGerada= document.getElementById('carta-gerada')
         for (let i=0 ;i<palavras.length;i+=1){
             let item=document.createElement('span')
+            item.addEventListener('click',alteraEstilo)
             item.innerText=palavras[i];
-            item.style.display='inline-block'
             let [est,tam,rot,inc]=selecionaEstilo()
             item.classList.add(grupoEstilo[est])
             item.classList.add(grupoTamanho[tam])
@@ -39,4 +48,14 @@ function criaCarta(){
             cartaGerada.appendChild(item)           
         }
     }
+}
+
+function alteraEstilo (event){
+    event.target.removeAttribute('class')
+    let [est,tam,rot,inc]=selecionaEstilo()
+    event.target.classList.add(grupoEstilo[est])
+    event.target.classList.add(grupoTamanho[tam])
+    event.target.classList.add(grupoRotacao[rot])
+    event.target.classList.add(grupoInclinacao[inc])
+
 }
