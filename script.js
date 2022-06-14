6eef3514b787fd5901b25a3eaf478f5b829025cc
Tomas Breuckmann <tomas.yoga@yahoo.com>
2022-06-14 18:49:29 -0300

    preparando para deploy e publicacao no github

--- script.js
+++ script.js
@@ -1,53 +1,48 @@
-const grupoEstilo = ['newspaper','magazine1','magazine2']
-const grupoTamanho = ['medium','big','reallybig']
-const grupoRotacao = ['rotateleft','rotateright']
-const grupoInclinacao =['skewleft','skewright']
+const grupoEstilo = ['newspaper', 'magazine1', 'magazine2'];
+const grupoTamanho = ['medium', 'big', 'reallybig'];
+const grupoRotacao = ['rotateleft', 'rotateright'];
+const grupoInclinacao = ['skewleft', 'skewright'];
 
-function selecionaEstilo(){
-    // let est = Math.floor(Math.random()*3)
-    // let tam = Math.floor(Math.random()*3)
-    // let rot = Math.floor(Math.random()*2)
-    // let inc = Math.floor(Math.random()*2)
-    let est = parseInt(Math.random()*3)
-    let tam = parseInt(Math.random()*3)
-    let rot = parseInt(Math.random()*2)
-    let inc = parseInt(Math.random()*2)
-
-    return [est,tam,rot,inc]
+function selecionaEstilo() {
+  const est = Math.floor(Math.random() * 3);
+  const tam = Math.floor(Math.random() * 3);
+  const rot = Math.floor(Math.random() * 2);
+  const inc = Math.floor(Math.random() * 2);
+  return [est, tam, rot, inc];
 }
 
-function apagaCarta(){
-    let itens=document.querySelectorAll('.palavra')
-    if (itens.length>0){
-        for(let i=itens.length-1;i>=0;i-=1){
-            itens[i].parentNode.removeChild(itens[i])
-        }
+function apagaCarta() {
+  const itens = document.querySelectorAll('.palavra');
+  if (itens.length > 0) {
+    for (let i = itens.length - 1; i >= 0; i -= 1) {
+      itens[i].parentNode.removeChild(itens[i]);
     }
+  }
 }
 
-function criaCarta(){
-    apagaCarta()
-    let cartaTexto=document.getElementById('carta-texto')
-    let conteudo=cartaTexto.value
-    if (conteudo==false){
-        document.getElementById('carta-gerada').innerText='Por favor, digite o conteúdo da carta.'
-    } else {
-        let palavras=conteudo.split(' ')
-        document.getElementById('carta-contador').innerText=palavras.length
-        let cartaGerada= document.getElementById('carta-gerada')
-        for (let i=0 ;i<palavras.length;i+=1){
-            let item=document.createElement('span')
-            item.addEventListener('click',alteraEstilo)
-            item.innerText=palavras[i];
-            let [est,tam,rot,inc]=selecionaEstilo()
-            item.classList.add(grupoEstilo[est])
-            item.classList.add(grupoTamanho[tam])
-            item.classList.add(grupoRotacao[rot])
-            item.classList.add(grupoInclinacao[inc])
-            item.classList.add('palavra')
-            cartaGerada.appendChild(item)           
-        }
+function criaCarta() {
+  apagaCarta();
+  const cartaTexto = document.getElementById('carta-texto');
+  const conteudo = cartaTexto.value.trim();
+  if (conteudo === false) {
+    document.getElementById('carta-gerada').innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    let palavras=conteudo.split(' ')
+    document.getElementById('carta-contador').innerText=palavras.length
+    let cartaGerada= document.getElementById('carta-gerada')
+    for (let i=0 ;i<palavras.length;i+=1){
+      let item=document.createElement('span')
+      item.addEventListener('click',alteraEstilo)
+      item.innerText=palavras[i];
+      let [est,tam,rot,inc]=selecionaEstilo()
+      item.classList.add(grupoEstilo[est])
+      item.classList.add(grupoTamanho[tam])
+      item.classList.add(grupoRotacao[rot])
+      item.classList.add(grupoInclinacao[inc])
+      item.classList.add('palavra')
+      cartaGerada.appendChild(item)
     }
+  }
 }
 
 function alteraEstilo (event){
